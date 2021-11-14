8397cd4fffa08256bb3ebde86904a55088222612
vlad_stoleru <vlad_stoleru@yahoo.com>
2021-11-14 21:45:49 +0200

    Add Lab2 demos

---
+++ Lab2/src/main/java/demo2/ApplicationConfiguration.java
@@ -0,0 +1,10 @@
+package demo2;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+@Configuration
+@ComponentScan(basePackages = "demo2")
+public class ApplicationConfiguration {}
---
+++ Lab2/src/main/java/demo6/President.java
@@ -0,0 +1,12 @@
+package demo6;
+
+public class President extends Person{
+    public President(String name) {
+        super(name);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("President %s", getName());
+    }
+}
